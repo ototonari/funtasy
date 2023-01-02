f9e245f7edc892454fa0a73fcf562a29362d91a2
Tsubasa Nagata <25589480+ototonari@users.noreply.github.com>
2023-01-02 23:33:14 +0900

    - enabled vercel audiences api

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { inject } from '@vercel/analytics';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from "recoil";
@@ -18,3 +19,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Vercel Audience API
+inject();
